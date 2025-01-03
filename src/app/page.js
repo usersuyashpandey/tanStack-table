7b6eadb2c6d1d213259680290d5ad0392fec2b4f
Suyash_Pandey <suyashpandey030@gmail.com>
2025-01-04 03:17:36 +0530

    Deployement Test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import { Providers } from "@/components/Providers";
 import { UserManagementTable } from "@/components/UserManagement";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <Providers>
       <main className="container mx-auto py-8">
-        <UserManagementTable />
+        <Suspense fallback={<div>Loading...</div>}>
+          <UserManagementTable />
+        </Suspense>
       </main>
     </Providers>
   );
